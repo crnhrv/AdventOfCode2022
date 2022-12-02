09d9c3e8996cba95f172062628bdc1df87659483
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-02 00:25:51 +0000

    make pylint happy

--- day1/solution.py
+++ day1/solution.py
@@ -1,18 +1,22 @@
-total_calories_per_elf = []
+def main():
+    total_calories_per_elf = []
+    calories = 0
+    with open("input.txt", encoding="utf8") as input_file:
+        calorie_chunks = input_file.read().splitlines()
+        for calories in calorie_chunks:
+            if not calories:
+                total_calories_per_elf.append(total_calories)
+                total_calories = 0
+            else:
+                total_calories += int(calories)
 
-with open("input.txt") as f:
-    input = f.read().splitlines()
-    total_calories = 0
-    for calories in input:
-        if not calories:
-            total_calories_per_elf.append(total_calories)
-            total_calories = 0
-        else:
-            total_calories += int(calories)
+    total_calories_per_elf.sort()
+    highest_total = total_calories_per_elf[-1]
+    print(highest_total)  # answer 1
 
-total_calories_per_elf.sort()
-highest_total = total_calories_per_elf[-1]
-print(highest_total)  # answer 1
+    top3 = total_calories_per_elf[-3:]
+    print(sum(top3))  # answer 2
 
-top3 = total_calories_per_elf[-3:]
-print(sum(top3))  # answer 2
+
+if __name__ == "__main__":
+    main()
