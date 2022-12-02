59d06bbb5d8d46ee689d8397004d0c97603d9cee
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-02 13:34:59 +0000

    add part 2 solution

--- day2/solution.py
+++ day2/solution.py
@@ -1,19 +1,51 @@
-def main():
-    points = {"X": 1, "Y": 2, "Z": 3}
-    win_key = {"X": "C", "Y": "A", "Z": "B"}
+def day2_part1_main(rounds):
+    move_points = {"X": 1, "Y": 2, "Z": 3}
+    key = {"X": "C", "Y": "A", "Z": "B"}
 
     total_score = 0
-    with open("input.txt", encoding="utf8") as input_file:
-        rounds = input_file.read().splitlines()
-        for rps_round in rounds:
-            (move, counter) = rps_round.split(" ")
-            total_score += points[counter]
-            if win_key[counter] is move:
-                total_score += 6
-            elif ord(counter) - ord(move) == 23:
-                total_score += 3
+    for rps_round in rounds:
+        (move, counter) = rps_round.split(" ")
+        total_score += move_points[counter]
+        if key[counter] is move:
+            total_score += 6
+        elif ord(counter) - ord(move) == 23:
+            total_score += 3
+
     print(total_score)  # answer 1
 
 
+def day2_part2_main(rounds):
+    move_points = {"A": 1, "B": 2, "C": 3}
+    key = {"A": "C", "B": "A", "C": "B"}
+
+    total_score = 0
+    for rps_round in rounds:
+        (move, result) = rps_round.split(" ")
+        if result == "Z":
+            total_score += 6
+            actual_counter = [
+                winning_move
+                for winning_move, losing_move in key.items()
+                if losing_move is move
+            ]
+            total_score += move_points[actual_counter[0]]
+        elif result == "Y":
+            total_score += 3
+            total_score += move_points[move]
+        else:
+            actual_counter = key[move]
+            total_score += move_points[actual_counter]
+
+    print(total_score)  # answer 2
+
+
+def read_input():
+    with open("input.txt", encoding="utf8") as input_file:
+        data = input_file.read().splitlines()
+    return data
+
+
 if __name__ == "__main__":
-    main()
+    rps_data = read_input()
+    day2_part1_main(rps_data)
+    day2_part2_main(rps_data)
