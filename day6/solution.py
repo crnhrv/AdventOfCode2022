6f326c63e2ad10917dec360e9026201d38a595f1
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-06 13:09:17 +0000

    add day6

---
+++ day6/solution.py
@@ -0,0 +1,30 @@
+def main(data):
+    for line in data:
+        print(get_marker(line, 4))
+        print(get_marker(line, 14))
+
+
+def get_marker(line, distinct):
+    index = 0
+    last_four = []
+    while len(last_four) != distinct:
+        if line[index + 1] == line[index]:
+            last_four = []
+        elif line[index] in [char for char, _ in last_four]:
+            dupe_index = next(i for (char, i) in last_four if char == line[index])
+            last_four = [(char, i) for (char, i) in last_four if i > dupe_index]
+
+        last_four.append((line[index], index))
+
+        index += 1
+    return index
+
+
+def read_input(filename):
+    with open(filename, encoding="utf8") as f:
+        return f.read().splitlines()
+
+
+if __name__ == "__main__":
+    dt = read_input("input.txt")
+    main(dt)
