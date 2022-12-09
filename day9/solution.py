155afa1981a35b7a628653ba038cf54cb3ba0605
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-09 18:50:30 +0000

    add part2 solution

--- day9/solution.py
+++ day9/solution.py
@@ -1,4 +1,4 @@
-def main(path):
+def part1(path):
     h_current = (0, 0)
     t_current = (0, 0)
     traversed = set()
@@ -25,8 +25,55 @@ def main(path):
     print(len(traversed))
 
 
+def part2(path):
+    rope = [(0, 0) for _ in range(10)]
+    traversed = set()
+    traversed.add((0, 0))
+    for (direction, distance) in path:
+        delta = (0, 0)
+        match direction:
+            case "U":
+                delta = (1, 0)
+            case "D":
+                delta = (-1, 0)
+            case "L":
+                delta = (0, -1)
+            case "R":
+                delta = (0, 1)
+        while distance:
+            distance -= 1
+            prev_tail = rope[-1]
+            rope = update_rope(rope, delta)
+            if prev_tail != rope[-1]:
+                traversed.add(rope[-1])
+
+    print(len(traversed))
+
+
+def update_rope(old_rope, delta):
+    new_rope = []
+    new_rope.append(update_position(old_rope[0], delta))
+    for (i, knot) in enumerate(old_rope[1:]):
+        new_head = new_rope[i]
+        new_tail = knot
+        if out_of_range(new_head, new_tail):
+            new_tail = find_new_position(new_head, new_tail)
+        new_rope.append(new_tail)
+    return new_rope
+
+
 def out_of_range(h, t):
     return abs(h[0] - t[0]) >= 2 or abs(h[1] - t[1]) >= 2
+
+
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
+
+
+def find_new_position(head, tail):
+    return update_position(
+        tail, (clamp(head[0] - tail[0], -1, 1), clamp(head[1] - tail[1], -1, 1))
+    )
 
 
 def update_position(current, delta):
@@ -46,4 +93,5 @@ def read_input(filename):
 
 if __name__ == "__main__":
     dt = read_input("input.txt")
-    main(dt)
+    part1(dt)
+    part2(dt)
