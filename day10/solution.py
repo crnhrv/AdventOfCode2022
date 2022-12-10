b19b091cffb4f5e4cb2a5815fee9ea29c27c6d5b
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-10 11:04:15 +0000

    add part1 solution

--- day10/solution.py
+++ day10/solution.py
@@ -1,10 +1,31 @@
 def main(data):
-    print(data)
+    register_at_cycle = dict()
+    cycle = 1
+    register = 1
+    for (cycle_count, to_add) in data:
+        register_at_cycle[cycle] = register
+        if cycle_count == 2:
+            register_at_cycle[cycle + 1] = register
+        cycle += cycle_count
+        register += to_add
+
+    answer1 = 0
+    important_cycles = [20, 60, 100, 140, 180, 220]
+    for cycle in important_cycles:
+        answer1 += register_at_cycle[cycle] * cycle
+    print(answer1)
 
 
 def read_input(filename):
     with open(filename, encoding="utf8") as f:
-        return f.read().splitlines()
+        data = []
+        for line in f.read().splitlines():
+            command = line.split(" ")
+            if len(command) == 1:
+                data.append((1, 0))
+            else:
+                data.append((2, int(command[1])))
+    return data
 
 
 if __name__ == "__main__":
