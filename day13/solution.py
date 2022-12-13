0075719da9454618e19dbeecbf1941c78b0f90b3
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-13 19:55:43 +0000

    pain

--- day13/solution.py
+++ day13/solution.py
@@ -1,18 +1,110 @@
 def main(data):
-    print(data)
+    i = 1
     ans1 = 0
-    ans2 = 0
+    for (left, right) in data:
+        if compare(left, right) == 1:
+            ans1 += i
+        i += 1
+    print(ans1)
+    packets = sort_packets(data)
+    l_divider = packets.index([[2]]) + 1
+    r_divider = packets.index([[6]]) + 1
+    ans2 = l_divider * r_divider
+    print(ans2)
 
-    return (ans1, ans2)
+
+def sort_packets(data):
+    packets = [item for sublist in data for item in sublist]
+    unsorted = True
+    while unsorted:
+        unsorted = False
+        i = 0
+        while i < len(packets) - 1:
+            if compare(packets[i + 1], packets[i]) == 1:
+                unsorted = True
+                temp = packets[i]
+                packets[i] = packets[i + 1]
+                packets[i + 1] = temp
+            i += 1
+    return packets
+
+
+def compare(l, r):
+    l_is_list = isinstance(l, list)
+    r_is_list = isinstance(r, list)
+    if l_is_list and r_is_list:
+        return compare_lists(l, r)
+    if l_is_list and not r_is_list:
+        r_list = [r]
+        return compare_lists(l, r_list)
+    if not l_is_list and r_is_list:
+        l_list = [l]
+        return compare_lists(l_list, r)
+    if l == r:
+        return 0
+    if l > r:
+        return -1
+    if l < r:
+        return 1
+
+
+def compare_lists(left, right):
+    i = 0
+    while i < len(left) and i < len(right):
+        value = compare(left[i], right[i])
+        if value != 0:
+            return value
+        i += 1
+
+    if len(left) == len(right):
+        return 0
+    if len(left) > len(right):
+        return -1
+    if len(left) < len(right):
+        return 1
 
 
 def read_input(filename):
     with open(filename, encoding="utf8") as f:
-        return f.read().splitlines()
+        lines = [l for l in f.read().splitlines() if l]
+        items = []
+        for item in lines:
+            if item[0] == "[":
+                new_list = []
+                items.append(convert_to_list(item[1:], new_list))
+        pairs = [items[i : i + 2] for i in range(0, len(items), 2)]
+        return pairs
+
+
+def convert_to_list(items, new_list):
+    if items[0] == "]":
+        return new_list
+    if items[0] == ",":
+        return convert_to_list(items[1:], new_list)
+    if items[0] == "'":
+        return convert_to_list(items[1:], new_list)
+    if items[0] == "[":
+        new_new_list = []
+        balance = 1
+        i = 1
+        while balance != 0:
+            if items[i] == "[":
+                balance += 1
+            elif items[i] == "]":
+                balance -= 1
+            i += 1
+        value = convert_to_list(items[1 : i + 1], new_new_list)
+    else:
+        i = 0
+        string = ""
+        while items[i].isnumeric():
+            string += items[i]
+            i += 1
+        value = int(string)
+    new_list.append(value)
+    return convert_to_list(items[i:], new_list)
 
 
 if __name__ == "__main__":
     dt = read_input("input.txt")
-    (ans1, ans2) = main(dt)
-    print(ans1)
-    print(ans2)
+    main(dt)
