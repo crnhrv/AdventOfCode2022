b8de8f48d6876fe022a7964a20e89dc45d206ecf
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-18 15:02:03 +0000

    add day18 p2

--- day18/solution.py
+++ day18/solution.py
@@ -7,6 +7,9 @@ class Cube:
     def __str__(self) -> str:
         return f"({self.x},{self.y},{self.z})"
 
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
     def __repr__(self) -> str:
         return f"{self}"
 
@@ -22,7 +25,60 @@ def main(cubes: list[Cube]):
                 == 1
             ):
                 total -= 2
+
     print(total)
+
+    min_x_cube = min(cubes, key=lambda x: x.x)
+    max_x_cube = max(cubes, key=lambda x: x.x)
+
+    min_y_cube = min(cubes, key=lambda x: x.y)
+    max_y_cube = max(cubes, key=lambda x: x.y)
+
+    min_z_cube = min(cubes, key=lambda x: x.z)
+    max_z_cube = max(cubes, key=lambda x: x.z)
+
+    minmaxes = (
+        (min_x_cube.x, max_x_cube.x),
+        (min_y_cube.y, max_y_cube.y),
+        (min_z_cube.z, max_z_cube.z),
+    )
+
+    deltas = [(0, 0, 1), (0, 0, -1), (0, 1, 0), (0, -1, 0), (1, 0, 0), (-1, 0, 0)]
+
+    all_cubes = set([(x.x, x.y, x.z) for x in cubes])
+    ans = 0
+    for cube in cubes:
+        for delta in deltas:
+            test_cube = (cube.x + delta[0], cube.y + delta[1], cube.z + delta[2])
+            if is_surface_cube(test_cube, minmaxes, all_cubes, deltas):
+                ans += 1
+    print(ans)
+
+
+def is_surface_cube(cube, minmaxes, all_cubes, deltas):
+    (x, y, z) = cube
+    seen = set()
+    stack = [(x, y, z)]
+    while stack:
+        (x, y, z) = stack.pop()
+        if (x, y, z) in seen:
+            continue
+        seen.add((x, y, z))
+        if (
+            x < minmaxes[0][0]
+            or x > minmaxes[0][1]
+            or x < minmaxes[1][0]
+            or x > minmaxes[1][1]
+            or x < minmaxes[2][0]
+            or x > minmaxes[2][1]
+        ):
+            return True
+        if (x, y, z) not in all_cubes:
+            for delta in deltas:
+                test_cube = (x + delta[0], y + delta[1], z + delta[2])
+                stack.append(test_cube)
+
+    return False
 
 
 def read_input(filename):
