2c060ff02d2d24641cc4300a28b9673026f8e4b6
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-20 16:16:54 +0000

    add part2

--- day20/solution.py
+++ day20/solution.py
@@ -1,27 +1,11 @@
-def part1(data):
-    data = mix(data[:], data[:])
-    data = [x[1] for x in data]
-    ans1 = get_answer(data)
-    print(ans1)
-
-
-def part2(data, dec_key):
-    data = [x * dec_key for x in data]
+def main(data, mix_count, dec_key):
+    data = [(i, x * dec_key) for (i, x) in enumerate(data)]
     ordering = data[:]
-    for _ in range(10):
+    for _ in range(mix_count):
         data = mix(data, ordering)
-        data = [x[1] for x in data]
-        print(data)
-    ans2 = get_answer(data)
-    print(ans2)
 
-
-def get_answer(data):
-    ans = 0
-    zero_index = data.index(0)
-    for i in range(1000, 3001, 1000):
-        index = (zero_index + i) % len(data)
-        ans += data[index]
+    data = [x[1] for x in data]
+    ans = get_answer(data)
     return ans
 
 
@@ -37,9 +21,17 @@ def mix(data: list[int], ordering):
 def shift(data: list[int], shift_by):
     index = data.index(shift_by)
     _ = data.pop(index)
-    new_index = (index + shift_by) % len(data)
-    new_value = (0, shift_by)
-    data.insert(new_index, new_value)
+    new_index = (index + shift_by[1]) % len(data)
+    data.insert(new_index, shift_by)
+
+
+def get_answer(data):
+    ans = 0
+    zero_index = data.index(0)
+    for i in range(1000, 3001, 1000):
+        index = (zero_index + i) % len(data)
+        ans += data[index]
+    return ans
 
 
 def read_input(filename):
@@ -48,6 +40,8 @@ def read_input(filename):
 
 
 if __name__ == "__main__":
-    dt = read_input("test-input.txt")
-    part1(dt)
-    part2(dt, 811589153)
+    dt = read_input("input.txt")
+    ans1 = main(dt, 1, 1)
+    ans2 = main(dt, 10, 811589153)
+    print(ans1)
+    print(ans2)
