b0793af8cc439a876abba91130a84f888c26ddd9
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-21 12:27:48 +0000

    add day21 boilerplate

---
+++ day21/solution.py
@@ -0,0 +1,12 @@
+def main(data):
+    print(data)
+
+
+def read_input(filename):
+    with open(filename, encoding="utf8") as input_file:
+        return list(map(int, input_file.read().splitlines()))
+
+
+if __name__ == "__main__":
+    dt = read_input("test-input.txt")
+    main(dt)
