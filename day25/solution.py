63d7cc839ec965658cd63f64cdf1f7913c0d3fe6
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-25 14:23:50 +0000

    tidy up code a bit

--- day25/solution.py
+++ day25/solution.py
@@ -22,53 +22,45 @@ def main(data, base):
         if snafu_number == total_real_number:
             snafu_digits.append("0")
         elif snafu_number > total_real_number:
-            lowest_potential_subtraction = (base**max_place) * -1
-            lowest_potential_new_snafu_number = (
-                snafu_number + lowest_potential_subtraction
-            )
-            lowest_difference = total_real_number - lowest_potential_new_snafu_number
-            highest_potential_subtraction = (base**max_place) * -2
-            highest_potential_new_snafu_number = (
-                snafu_number + highest_potential_subtraction
-            )
-            highest_difference = total_real_number - highest_potential_new_snafu_number
+            min_subtraction = (base**max_place) * -1
+            min_new_snafu = snafu_number + min_subtraction
+            min_diff = total_real_number - min_new_snafu
+
+            max_subtraction = (base**max_place) * -2
+            max_new_snafu = snafu_number + max_subtraction
+            max_diff = total_real_number - max_new_snafu
+
             next_max_addition = (base ** (max_place - 1)) * 2
 
-            if (
-                highest_difference > next_max_addition
-                and lowest_difference > next_max_addition
-            ):
+            if max_diff > next_max_addition and min_diff > next_max_addition:
                 snafu_digits.append("0")
-            elif highest_difference > next_max_addition:
+            elif max_diff > next_max_addition:
                 snafu_digits.append("-")
-                snafu_number += lowest_potential_subtraction
+                snafu_number = min_new_snafu
             else:
                 snafu_digits.append("=")
-                snafu_number += (base**max_place) * -2
+                snafu_number = max_new_snafu
         elif snafu_number < total_real_number:
-            max_next_subtraction = (base ** (max_place - 1)) * 2
-            lowest_addition = (base**max_place) * 1
-            lowest_new_snafu_number = snafu_number + lowest_addition
-            min_required_to_subtract_after_this = (
-                lowest_new_snafu_number - total_real_number
-            )
+            next_max_subtraction = (base ** (max_place - 1)) * 2
 
-            highest_addition = (base**max_place) * 2
-            highest_new_snafu_number = snafu_number + highest_addition
-            max_required_to_subtract_after_this = (
-                highest_new_snafu_number - total_real_number
-            )
+            min_addition = (base**max_place) * 1
+            min_new_snafu = snafu_number + min_addition
+            min_required_subtraction = min_new_snafu - total_real_number
+
+            max_addition = (base**max_place) * 2
+            max_new_snafu = snafu_number + max_addition
+            max_required_subtraction = max_new_snafu - total_real_number
             if (
-                min_required_to_subtract_after_this > max_next_subtraction
-                and max_required_to_subtract_after_this > max_next_subtraction
+                min_required_subtraction > next_max_subtraction
+                and max_required_subtraction > next_max_subtraction
             ):
                 snafu_digits.append("0")
-            elif max_required_to_subtract_after_this > max_next_subtraction:
+            elif max_required_subtraction > next_max_subtraction:
                 snafu_digits.append("1")
-                snafu_number += (base**max_place) * 1
+                snafu_number = min_new_snafu
             else:
                 snafu_digits.append("2")
-                snafu_number += (base**max_place) * 2
+                snafu_number = max_new_snafu
         max_place -= 1
     print("".join(snafu_digits))
 
