f2e1b8706117ee32df5dae9d177ed5f29a30f9c5
Ciaran Harvey <ciaran.harvey@red-gate.com>
2022-12-25 02:19:02 +0000

    add day 24 p1

--- day24/solution.py
+++ day24/solution.py
@@ -20,51 +20,68 @@ class Directions:
 
 
 def main(data):
-    (current_position, ending_spot, available_spots, walls, blizzards) = data
-    distance_to_end = get_manhattan_distance(current_position, ending_spot)
+    (current_position, ending_spot, _, walls, blizzards) = data
     directions = [Directions.U, Directions.L, Directions.D, Directions.R]
-    while distance_to_end > 0:
-        current_blizzard_positions = set()
-        new_blizzards = []
-        for (blizzard, direction, spawn_point) in blizzards:
-            new_blizzard_position = move(blizzard, direction)
-            if new_blizzard_position in walls:
-                new_blizzard = (spawn_point, direction, spawn_point)
-                current_blizzard_positions.add(spawn_point)
-            else:
-                new_blizzard = (new_blizzard_position, direction, spawn_point)
-                current_blizzard_positions.add(new_blizzard_position)
-            new_blizzards.append(new_blizzard)
+    max_x = max(w[0] for w in walls) - 1
+    max_y = max(w[1] for w in walls) - 2
+    minutes = 0
+    positions = [(current_position, minutes)]
+    quickest = float("inf")
+    stack = collections.deque(positions)
+    blizzard_cache = {}
+    seen = set()
 
-        potential_moves = []
-        for direction in directions:
-            new_position = move(current_position, direction)
-            if new_position in (available_spots - current_blizzard_positions):
-                potential_moves.append(new_position)
+    while stack:
+        (current_position, minutes) = stack.popleft()
+        if minutes > quickest:
+            continue
+        if current_position == ending_spot:
+            quickest = min(minutes, quickest)
+            continue
 
-        potential_moves.sort(key=lambda x: get_manhattan_distance(x, ending_spot))
+        if minutes in blizzard_cache:
+            current_blizzards = blizzard_cache[minutes]
+        else:
+            new_blizzards = []
+            for (blizzard, direction) in blizzards.copy():
+                new_bpos = move_blizzard(blizzard, direction, max_x, max_y)
+                new_blizzards.append((new_bpos, direction))
+            blizzards = new_blizzards
+            current_blizzards = set((x[0] for x in blizzards))
+            blizzard_cache[minutes] = current_blizzards
+        key = (current_position, minutes)
+        if key in seen:
+            continue
+        seen.add(key)
+        potentials = set(
+            [move(current_position, direction) for direction in directions]
+        )
+        new_locations = potentials - current_blizzards - walls
 
-        # make this dfs?
+        for new_location in new_locations:
+            stack.append((new_location, minutes + 1))
+
+        if not new_locations:
+            stack.append((current_position, minutes + 1))
+
+    print(quickest)
 
 
 def move(position, direction):
     return (position[0] + direction[0], position[1] + direction[1])
 
 
-def get_manhattan_distance(position: tuple[int, int], target_position: tuple[int, int]):
-    return abs(position[0] - target_position[0]) + abs(position[1] - target_position[1])
-
-
-def get_blizzard_spawn(blizzard, direction, max_x, max_y):
-    match direction:
-        case Directions.L:
-            return (max_x, blizzard[1])
-        case Directions.R:
-            return (1, blizzard[1])
-        case Directions.U:
-            return (blizzard[0], max_y)
-        case Directions.D:
-            return (blizzard[0], 1)
+def move_blizzard(position, direction, max_x, max_y):
+    new_pos = move(position, direction)
+    if new_pos[0] < 1:
+        return (max_x + 1, new_pos[1])
+    if new_pos[0] > max_x:
+        return (1, new_pos[1])
+    if new_pos[1] < 1:
+        return (new_pos[0], max_y - 2)
+    if new_pos[1] > max_y:
+        return (new_pos[0], 1)
+    return new_pos
 
 
 def read_input(filename):
@@ -80,22 +97,22 @@ def read_input(filename):
                 if spot == ".":
                     if y == 0:
                         starting_spot = (x, y)
+                        walls.add((x, y - 1))
+                        continue
                     if y == len(lines) - 1:
                         ending_spot = (x, y)
+                        walls.add((x, y + 1))
                     available_spots.add((x, y))
                 elif spot == "#":
                     walls.add((x, y))
                 else:
                     direction = Directions.convert_direction(spot)
-                    starting_position = get_blizzard_spawn(
-                        (x, y), direction, len(line) - 2, len(lines) - 2
-                    )
-                    blizzards.append(((x, y), direction, starting_position))
+                    blizzards.append(((x, y), direction))
                     available_spots.add((x, y))
 
     return (starting_spot, ending_spot, available_spots, walls, blizzards)
 
 
 if __name__ == "__main__":
-    dt = read_input("sample-input.txt")
+    dt = read_input("input.txt")
     main(dt)
